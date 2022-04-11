04ccc2e949e044944dbd497f33203e66691aff7d
liyche <liyche@umich.edu>
2022-04-10 21:58:37 -0400

    Added icons to images folder and links in manifest.json

--- js/main.js
+++ js/main.js
@@ -1,7 +1,36 @@
-const buttons = document.querySelectorAll('input[name="speed"]');
+let vidDuration;    
 
-Array.from(buttons).forEach(button => button.addEventListener('change', calcTime));
+async function injectFunc(func) {
+    let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
 
-function calcTime(click) {
-    document.querySelector('#time').innerText = click.target.value;
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: func
+    });
 }
+
+function getDuration() {
+    let duration = document.querySelector('video').duration;
+    chrome.storage.sync.set({ duration });
+}
+
+function calcTime(evt) {
+    document.querySelector('#time').innerText = vidDuration / evt.target.value;
+}
+
+function setUpEvent() {
+    // add event listeners to speed option buttons
+    const buttons = document.querySelectorAll('input[name="speed"]');
+    Array.from(buttons).forEach(button => button.addEventListener('change', calcTime));
+
+    chrome.storage.sync.get("duration", ({ duration }) => {
+        vidDuration = duration;
+        document.querySelector('#time').innerText = duration;
+    })
+}
+
+// when pop up is opened, get video duration
+let durationPromise = injectFunc(getDuration);
+
+// calculate video duration under different speeds
+durationPromise.then(setUpEvent);
