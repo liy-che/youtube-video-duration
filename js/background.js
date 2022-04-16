e39dfb95cc4429884d39d4fd7d23c3dec515cfcd
liyche <liyche@umich.edu>
2022-04-16 12:08:39 -0400

    add declarativeContent

--- js/background.js
+++ js/background.js
@@ -1,6 +1,23 @@
-chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: {tabId: tab.id},
-    files: ['js/content.js']
+console.log('hi it\'s background');
+
+chrome.runtime.onInstalled.addListener(() => {
+  // Page actions are disabled by default and enabled on select tabs
+  chrome.action.disable();
+
+  // Clear all rules to ensure only our expected rules are set
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    // Declare a rule to enable the action on example.com pages
+    let exampleRule = {
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {urlContains: 'www.youtube.com/watch?v='},
+        })
+      ],
+      actions: [new chrome.declarativeContent.ShowAction()],
+    };
+
+    // Finally, apply our new array of rules
+    let rules = [exampleRule];
+    chrome.declarativeContent.onPageChanged.addRules(rules);
   });
 });
