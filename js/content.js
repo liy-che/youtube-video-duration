6bf76cbcf96bd0e787723aad81d74c4dc62ce40e
liyche <liyche@umich.edu>
2022-04-17 00:14:55 -0400

    Add keyboard shortcuts

--- js/content.js
+++ js/content.js
@@ -7,9 +7,20 @@ if(document.readyState === 'loading') {
 
 function afterDOMLoaded(){
     //Everything that needs to happen after the DOM has initially loaded.
-    let duration = document.querySelector('video').duration;
+    let video = document.querySelector('video')
+    let duration = video.duration;
     let title = document.querySelector('h1 yt-formatted-string.style-scope.ytd-video-primary-info-renderer').innerText;
-    chrome.runtime.sendMessage({msgType: 1, vidTitle: title, durationInSec: duration}, function(response) {
+    chrome.runtime.sendMessage({msgType: 1, vidTitle: title, durationInSec: duration, speed: video.playbackRate}, function(response) {
         console.log(response.farewell);
     });
 }
+
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.msgType === 1) {
+            document.querySelector('video').playbackRate = request.speed;
+        }
+        sendResponse({farewell: "goodbye"});
+    }
+);
--- js/main.js
+++ js/main.js
@@ -1,26 +1,51 @@
-let vidDuration;
+/******************************* program states *******************************/
 
+
+let vidDuration;
+let speedChosen = 1.0;
+
+
+/******************************* event listeners ******************************/
+
+
+// listen to message from content script
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      if (request.msgType === 1) {
+          document.querySelector('h1').innerText = request.vidTitle;
+          vidDuration = request.durationInSec;
+          updateCalcResult(vidDuration/request.speed);
+          updateChecked(request.speed);
+          sendResponse({farewell: "goodbye"});
+      }
+    }
+);
+
+// calculate time for selected speed
 const buttons = document.querySelectorAll('input[name="speed"]');
 Array.from(buttons).forEach(button => button.addEventListener('change', calcTime));
 
-async function injectScript() {
-    let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
-
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        files: ['js/content.js']
-    });
-}
-
 function calcTime(evt) {
-    const newTime = vidDuration / evt.target.value;
-    updateCalcResult(newTime, vidDuration-newTime);
+    speedChosen = evt.target.value; 
+    const newTime = vidDuration / speedChosen;
+    updateCalcResult(newTime);
 }
 
-function updateCalcResult(newTime, saved=0) {
-    document.querySelector('#time').innerText = convertSecondToTimestamp(newTime);
-    document.querySelector('#saved').innerText = convertSecondToTimestamp(saved);
+// listening for enter key up on radio buttons
+const radioButtons = document.querySelectorAll('input[name="speed"]');
+Array.from(radioButtons).forEach(button => button.addEventListener("keyup", keyPressHandler));
+
+function keyPressHandler(evt) {
+    if (evt.keyCode === 13) {
+        evt.preventDefault();
+        sendMessage({msgType: 1, speed: speedChosen});
+    } 
+    else if (evt.keyCode === 32) window.close();
 }
+
+
+/******************************* helper functions *****************************/
+
 
 function zeroPad(num, numFigure=2, char='0') {
     // assume num is a number
@@ -39,19 +64,59 @@ function convertSecondToTimestamp(totalSeconds) {
     return timeStr;
 }
 
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      console.log(sender.tab ?
-                  "from a content script:" + sender.tab.url :
-                  "from the extension");
-      if (request.msgType === 1) {
-          document.querySelector('h1').innerText = request.vidTitle;
-          vidDuration = request.durationInSec;
-          updateCalcResult(vidDuration);
-          sendResponse({farewell: "goodbye"});
-      }
+
+/********************************* functions **********************************/
+
+
+function sendMessage(msg) {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, msg, function(response) {
+          console.log(response.farewell);
+        });
+    });
+}
+
+function updateCalcResult(newTime) {
+    const diffWord = document.querySelector('#diff-word');
+    const diff = newTime - vidDuration
+    const timeDiff = newTime - vidDuration;
+
+    document.querySelector('#time').innerText = convertSecondToTimestamp(newTime);
+    document.querySelector('#diff').innerText = convertSecondToTimestamp(Math.abs(timeDiff));
+
+    if (timeDiff < 0) diffWord.innerText = 'Saved: ';
+    else if (timeDiff > 0) diffWord.innerText = 'Added: ';
+    else {
+        diffWord.innerText = '';
+        document.querySelector('#diff').innerText = '';
     }
-);
+}
+
+function updateChecked(curSpeed) {
+    if (curSpeed != speedChosen) {
+        speedChosen = curSpeed;
+        const curSelected = document.querySelector('input[checked]');
+        curSelected.removeAttribute('checked');
+        curSelected.removeAttribute('autofocus');
+
+        // update selection
+        const newSelected = document.querySelector(`input[value="${curSpeed}"]`);
+        newSelected.setAttribute('checked', '');
+        newSelected.focus();
+    }
+}
+
+async function injectScript() {
+    let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ['js/content.js']
+    });
+}
+
+
+/******************************* main program *********************************/
 
 // when pop up is opened, get video duration
 injectScript();
