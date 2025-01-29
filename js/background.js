3f0aec666ca489d25c4e08f68bf50488978ab855
liyche <liyche@umich.edu>
2025-01-29 13:38:15 -0500

    temporarily remove inject on install/update to avoid duplicate

--- js/background.js
+++ js/background.js
@@ -1,25 +1,6 @@
 // runs on extension install, extension update, and browser update
 // does not run on extension enable
 chrome.runtime.onInstalled.addListener((details) => {
-  // re-inject content scripts and set badge to matches after install or update
-  for (const cs of chrome.runtime.getManifest().content_scripts) {
-    chrome.tabs.query({url: cs.matches}, (tabs)=> {
-      for (const tab of tabs) {
-        chrome.scripting.executeScript({
-          target: {tabId: tab.id},
-          files: cs.js,
-        }).then(() => {
-          chrome.tabs.sendMessage(tab.id, { msgType: "inject" });
-        });
-
-        chrome.scripting.insertCSS({
-          target: {tabId: tab.id},
-          files: cs.css,
-        });
-
-      }
-    });
-  }
 
   if (details.reason === 'update' || details.reason === 'install') {
     chrome.storage.sync.set({ opened: false });
